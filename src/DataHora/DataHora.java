f698693db7d88d9d2e1ca790640da04d7eca849d
Charles <charlesthiagosantos@gmail.com>
2023-12-18 06:16:04 -0300

    feat : instanciação com o modelo ISO 8601

--- src/DataHora/DataHora.java
+++ src/DataHora/DataHora.java
@@ -15,6 +15,18 @@ public class DataHora {
         Instant instant01 = Instant.now();// Horario em GMT/UTC = Horario em londres
         System.out.printf("Instant01- %s\n", instant01);
 
+        System.out.println("--------------------------------------");
+
+        // Texto ISO 8601 (parse)
+        LocalDate localDate02 = LocalDate.parse("2023-12-18");
+        System.out.printf("LocalDate02- %s\n",localDate02);
+        LocalDateTime localDateTime02 = LocalDateTime.parse("2023-12-18T05:52:33");
+        System.out.printf("LocalDateTime02- %s\n", localDateTime02);
+        Instant instant02 = Instant.parse("2023-12-18T05:52:33Z");
+        System.out.printf("Instant02- %s\n", instant02);
+        Instant instantZulu = Instant.parse("2023-12-18T05:52:33-03:00");
+        System.out.printf("InstantZulu- %s\n", instantZulu); // soma ou subtração da hora
+
 
 
     }
