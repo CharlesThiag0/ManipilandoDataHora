c3039c1dcd9c9de070c0690569d7d6515537f3a1
Charles <charlesthiagosantos@gmail.com>
2023-12-20 06:21:38 -0300

    feat : horario global em formato custom e tranformando em horarios por ZoneId

--- src/DataHora/GlobalLocal.java
+++ src/DataHora/GlobalLocal.java
@@ -1,26 +1,31 @@
 package DataHora;
 
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class GlobalLocal {
     public static void main(String[] args) {
-        Instant instant = Instant.parse("2023-12-31T01:30:26Z");
+        // criação do horario gtm e local
+        Instant instant = Instant.parse("2024-01-01T01:30:45Z");
         LocalDateTime localDateTime = LocalDateTime.now();
-
-        System.out.println(LocalDate.ofInstant(instant, ZoneId.systemDefault()));
-        System.out.println(LocalDate.ofInstant(instant, ZoneId.of("Portugal")));
-
-        System.out.println(LocalDateTime.ofInstant(instant, ZoneId.systemDefault()));
-        System.out.println(LocalDateTime.ofInstant(instant, ZoneId.of("Portugal")));
-
-        System.out.println(localDateTime.getDayOfMonth());
-        System.out.println(localDateTime.getMonthValue());
-        System.out.println(localDateTime.getYear());
-        System.out.println(localDateTime.getHour());
-        System.out.println(localDateTime.getMinute());
+        //formato custom
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd//MM//yyyy HH,mm");
+        //tranforma hora global em hora local
+        System.out.printf("Horario Local: %s\n", formato.withZone(ZoneId.systemDefault()).format(instant));
+        // tranforma hora global em hora da egito
+        System.out.printf("Horario em Egito: %s\n", formato.withZone(ZoneId.of("Egypt"))
+                .format(instant));
+        //tranforma hora global em hora de portugal
+        System.out.printf("Horario em Portugal: %s\n", formato.withZone(ZoneId.of("Portugal"))
+                .format(instant));
+        // pegar dia mes ano hora e minutos
+        System.out.printf("Minutos: %s\n", localDateTime.getMinute());
+        System.out.printf("Horas: %s\n", localDateTime.getHour());
+        System.out.printf("Dia: %s\n", localDateTime.getDayOfMonth());
+        System.out.printf("Mês: %s\n", localDateTime.getMonthValue());
+        System.out.printf("Ano: %s\n", localDateTime.getYear());
 
     }
 }
